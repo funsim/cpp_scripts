68746de3615d8bfe35f4d4eb042b58ef18ed9313
Simon Funke <funsi@gmx.de>
2012-05-06 00:27:04 +0100

    A first working version

--- db_scheme.sql
+++ db_scheme.sql
@@ -1,6 +1,9 @@
 CREATE TABLE IF NOT EXISTS `mlab` (
+  `id` int NOT NULL AUTO_INCREMENT, 
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `date` datetime NOT NULL,
   `destination` varchar(128) NOT NULL,
-  `source` varchar(128) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `source` varchar(128) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
