0c17fe55bd6dee38f56e742f6e8b9279d77fe243
Simon Funke <funsi@gmx.de>
2012-05-09 01:14:00 +0100

    1) Store different testsuits in different tables (currently only ndt and glosnost) 2) Store the filename with each connection.

--- db_scheme.sql
+++ db_scheme.sql
@@ -1,9 +1,25 @@
-CREATE TABLE IF NOT EXISTS `mlab` (
+CREATE TABLE IF NOT EXISTS `glasnot` (
   `id` int NOT NULL AUTO_INCREMENT, 
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `date` datetime NOT NULL,
   `destination` varchar(128) NOT NULL,
   `source` varchar(128) NOT NULL,
+  `file_id` int NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
+CREATE TABLE IF NOT EXISTS `ndt` (
+  `id` int NOT NULL AUTO_INCREMENT, 
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date` datetime NOT NULL,
+  `destination` varchar(128) NOT NULL,
+  `source` varchar(128) NOT NULL,
+  `file_id` int NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `files` (
+  `id` int NOT NULL AUTO_INCREMENT, 
+  `filename` varchar(256) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
