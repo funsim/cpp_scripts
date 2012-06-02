7d5d9811eac1eccd758cf667021bfb28c35129c0
Simon Funke <funsi@gmx.de>
2012-06-02 18:07:47 -0300

    Use country_code instead of source in the multilevel databases

--- create_multilevel_db.sql
+++ create_multilevel_db.sql
@@ -5,11 +5,11 @@ CREATE TABLE IF NOT EXISTS `glasnost_daily` (
   `counter` int NOT NULL, 
   `rangedate` datetime NOT NULL,
   `destination` varchar(128) NOT NULL,
-  `source` varchar(128) NOT NULL,
-  PRIMARY KEY (rangedate, destination, source)
+  `country_code` varchar(128) NOT NULL,
+  PRIMARY KEY (rangedate, destination, country_code)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
-INSERT INTO glasnost_daily SELECT COUNT(*) as counter, DATE(date) as rangedate, destination, source FROM glasnost GROUP BY destination, source, DATE(rangedate);
+INSERT INTO glasnost_daily SELECT COUNT(*) as counter, DATE(date) as rangedate, destination, country_code FROM glasnost GROUP BY destination, country_code, DATE(rangedate);
 
 /* glasnost_monthly */
 DROP TABLE IF EXISTS `glasnost_monthly`;
@@ -18,11 +18,11 @@ CREATE TABLE IF NOT EXISTS `glasnost_monthly` (
   `counter` int NOT NULL, 
   `rangedate` datetime NOT NULL,
   `destination` varchar(128) NOT NULL,
-  `source` varchar(128) NOT NULL,
-  PRIMARY KEY (rangedate, destination, source)
+  `country_code` varchar(128) NOT NULL,
+  PRIMARY KEY (rangedate, destination, country_code)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
-INSERT INTO glasnost_monthly SELECT COUNT(*) as counter, LAST_DAY(date) as rangedate, destination, source FROM glasnost GROUP BY destination, source, LAST_DAY(rangedate);
+INSERT INTO glasnost_monthly SELECT COUNT(*) as counter, LAST_DAY(date) as rangedate, destination, country_code FROM glasnost GROUP BY destination, country_code, LAST_DAY(rangedate);
 
 /* glasnost_yearly */
 DROP TABLE IF EXISTS `glasnost_yearly`;
@@ -31,8 +31,8 @@ CREATE TABLE IF NOT EXISTS `glasnost_yearly` (
   `counter` int NOT NULL, 
   `rangedate` datetime NOT NULL,
   `destination` varchar(128) NOT NULL,
-  `source` varchar(128) NOT NULL,
-  PRIMARY KEY (rangedate, destination, source)
+  `country_code` varchar(128) NOT NULL,
+  PRIMARY KEY (rangedate, destination, country_code)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
-INSERT INTO glasnost_yearly SELECT COUNT(*) as counter, MAKEDATE(YEAR(date), 1) as rangedate, destination, source FROM glasnost GROUP BY destination, source, MAKEDATE(YEAR(rangedate), 1);
+INSERT INTO glasnost_yearly SELECT COUNT(*) as counter, MAKEDATE(YEAR(date), 1) as rangedate, destination, country_code FROM glasnost GROUP BY destination, country_code, MAKEDATE(YEAR(rangedate), 1);
